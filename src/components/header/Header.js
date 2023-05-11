a93b9fc195fe105f99a50623dda54ee52b3a2c7c
sureshkanna1071 <sureshkanna.p1071@gmail.com>
2023-05-11 18:50:05 +0530

    Add geolocation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import "./header.css"
 import logos from "../../assets/logos.png"
 import { IconButton } from '@mui/material';
@@ -9,6 +9,28 @@ import Modal from '../modal/Modal';
 
 const Header = () => {
   const {theme, toggleTheme} = useContext(ThemeContext);
+  const [temp, setTemp] = useState("");
+
+  useEffect(() => {
+    if(navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(getWeatherStats);
+    } else {
+      return
+    }
+  }, [])
+
+  const getWeatherStats = (data) => {
+    let lat = data.coords.latitude
+    let lon = data.coords.longitude
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`; 
+     fetch(url, {method: 'get'})
+     .then((res) => res.json())
+     .then((res) => {
+      setTemp({temp: res.list[0].temp.day, city: res.city.name})
+     })
+  }
+ 
+  console.log(temp)
 
   return (
     <>
@@ -20,6 +42,7 @@ const Header = () => {
         <button className='br__header_container_button'>SHOP</button>
         <h3 className='br__header_container-link'>Partner with Us</h3>
           <IconButton onClick={() => toggleTheme()} sx={{marginLeft: "25px"}} >{theme === "light" ? <LightModeIcon sx={{color: "#07219B"}} /> : <DarkModeIcon sx={{color: "#07219B"}} />}</IconButton>
+          <h4 className='br__header_temp'>{temp.temp ? `${temp.temp}°C ${temp.city}` : null}</h4>
       </div>
     </div>
     <Modal />
